d0ef7eeaacf5304987c14f33b5b779659675d294
BATARD Kevin (SNCF RESEAU / EIC AQUITAINE POITOU-CHARENTES / CIRC BAYONNE) <9503305e@commun.ad.sncf.fr>
2022-11-30 12:24:16 +0100

    feat ajout class character

---
+++ src/character.ts
@@ -0,0 +1,71 @@
+export class Character {
+    private name: string;
+    private health: number;
+    private strength: number;
+    private lvl: number ;
+    private xp: number ;
+
+    constructor(name: string, health: number, strength: number, lvl: number, xp: number) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength
+        this.lvl = lvl;
+        this.xp = xp;
+    }
+
+    getName(): string {
+        return this.name
+    }
+
+    setName(newName: string): string {
+        this.name = newName
+        return newName
+    }
+
+    getHealth(): number {
+        return this.health;
+    }
+
+    setHealth(newHealth: number): number {
+        this.health = newHealth;
+        return newHealth;
+    }
+
+    getStrength(): number { 
+        return this.strength;
+    }
+
+    setStrength(newStrength: number): number {
+        this.strength = newStrength;
+        return newStrength;
+    }
+
+    getLvl(): number {
+        return this.lvl;
+    }
+
+    setLvl(newLvl: number): number {
+        this.lvl = newLvl;
+        return newLvl;
+    }
+
+    getXP(): number {
+        return this.xp;
+    }
+
+    setXp(newXp: number): number {
+        this.xp = newXp;
+        return newXp;
+    }
+
+    attack(opponent: Hero | Enemy): void {
+        opponent.health -= this.strength;
+
+
+    }
+
+    die(): Boolean {
+        if (this.health <= 0) { return true }
+        else { return false }
+    };
+    }
