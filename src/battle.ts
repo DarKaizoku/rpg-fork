6526a67a7777534371be2dce49e52440c191700d
WAM <eraserw@gmail.com>
2022-12-01 14:18:58 +0100

    merge011222-1418

--- src/battle.ts
+++ src/battle.ts
@@ -17,21 +17,23 @@ export class Battle {
     }
     
 
-    /* affichageTour(tableau : any,tour:number) {
+      affichageTour(tableau : any,tour:number) {
 
-        let affichage = document.getElementById('???');
+        let affichage = document.getElementById('combat');
         const newRow1 = document.createElement("tr");
         const newCaseRound = document.createElement(`"th class="pt-4" scope="row" rowspan="2"`);
         newCaseRound.textContent = `${tour}`;
-        const newCaseAction1 = document.createElement(`Cloud attaque en 1er avec strength`);
+        const newCaseAction1 = document.createElement("td");
         newCaseAction1.textContent = `Cloud attaque en 1er avec strength`;
-        const 
-        newRow.appendChild(newcase);
-        newRow.appendChild(addCase(idUser));
-        newRow.appendChild(addCase(input));
-        newRow.appendChild(addBin(idTicket));
-        affichage.appendChild(newRow);
-    } */
+        const newRow2 = document.createElement("tr");
+        const newCaseAction2 = document.createElement("td");
+        newCaseAction2.textContent = `test 1 2 Test..`;
+        newRow1.appendChild(newCaseRound);
+        newRow1.appendChild(newCaseAction1);
+        affichage.appendChild(newRow1);
+        newRow2.appendChild(newCaseAction2);
+        affichage.appendChild(newRow2);
+    } 
     
     declare(): void {
 
@@ -45,37 +47,32 @@ export class Battle {
             const tableTour = [2, 4, 6];
             //while (this.hero.getHealth() > 0 && this.enemy.getHealth() > 0) {
             this.hero.attack(this.enemy);
+            this.affichageTour(this.hero.stat(),tour);
+            ++tour;
+
+
             if(this.enemy instanceof Assassin){
                 this.enemy.attackAssassin(this.hero);
             }
-            this.enemy.attack(this.hero);
-            ++tour;
-            //this.affichageTour(this.hero.stat(),tour);
 
 
 
-            /* if ( !(this.enemy instanceof (Dragon || Griffin))) {
-                this.enemy.attack(this.hero);
-            }
-            else {
-                if (tour === 2) {
-                    this.enemy.attack(this.hero);
-                }
-                else if (tour === 4){
+            if (this.enemy instanceof (Dragon || Griffin)) {
+                if (tour === 4){
                     this.enemy.flight();
                 }
-                else {
+                else if (tour === 6){
                     this.enemy.attackFromSky(this.hero);
                 }
             }
-            //this.affichageTour(this.enemy.stat(),tour);
+            this.enemy.attack(this.hero);
+            this.affichageTour(this.enemy.stat(),tour);
             ++tour;
             
-            if (tour === 6) {
+            if (tour > 6) {
                 tour = initTour
-            } */
+            }
 
-            
             console.log(this.hero, this.enemy);
         }
         //console.log(this.hero, this.enemy);
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { Werewolf } from "./werewolf";
 
 let jeanPierre = new Hero("Jean-Pierre",80,20,false,0,0,"human");
 
-let drake = new Assassin("ChienDenté",50,10,false,0,0);
+let drake = new Dragon("ChienDenté",50,10,false,0,0);
 
 
 let battleTest = new Battle(jeanPierre,drake);
